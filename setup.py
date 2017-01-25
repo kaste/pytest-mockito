c0d76670aa148bfa864397cdf7b52eb885cc6753
herr kaste <herr.kaste@gmail.com>
2017-01-25 18:01:06 +0100

    Bump version 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pytest-mockito",
-    version='0.0.1',
+    version='0.0.2',
     description='Base fixtures for mockito',
     long_description=open('README.rst').read(),
     license='MIT',
